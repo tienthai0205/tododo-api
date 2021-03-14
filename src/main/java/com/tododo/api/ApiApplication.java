da85a25896a612ae279a702463303070cce75cfe
Thai Thuy Tien <tienthai0205>
2021-03-14 20:28:22 +0100

    added docker wip

--- src/main/java/com/tododo/api/ApiApplication.java
+++ src/main/java/com/tododo/api/ApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class ApiApplication {
