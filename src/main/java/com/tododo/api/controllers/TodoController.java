096981c96cc9da5692163d4ad503d0c217a49c48
Thai Thuy Tien <tienthai0205>
2021-03-20 01:57:11 +0100

    added put and delete apis

--- src/main/java/com/tododo/api/controllers/TodoController.java
+++ src/main/java/com/tododo/api/controllers/TodoController.java
@@ -11,8 +11,10 @@ import com.tododo.api.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -36,12 +38,16 @@ public class TodoController {
     public ResponseEntity<?> getTodoList(Principal principal) {
 
         List<Todo> todos = todoRepository.findByUserId(currentUser(principal).getId());
+
         return ResponseEntity.ok(todos);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getTodoItem(Principal principal, @PathVariable int id) {
         Todo todo = todoRepository.findById(id);
+        if (todo == null) {
+            return new ResponseEntity<>("Todo item with id " + id + " not found", HttpStatus.NOT_FOUND);
+        }
 
         if (todo.getUser().getId() != currentUser(principal).getId()) {
             return new ResponseEntity<>("You are not authorized to retrieved this item", HttpStatus.FORBIDDEN);
@@ -56,6 +62,42 @@ public class TodoController {
         newTodo.setUser(currentUser(principal));
         todoRepository.save(newTodo);
         return new ResponseEntity<>(newTodo, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTodoItem(Principal principal, @PathVariable int id, @RequestBody Todo updateTodo) {
+        Todo todo = todoRepository.findById(id);
+
+        if (todo == null) {
+            return new ResponseEntity<>("Todo item with id " + id + " not found", HttpStatus.NOT_FOUND);
+        }
+
+        if (todo.getUser().getId() != currentUser(principal).getId()) {
+            return new ResponseEntity<>("You are not authorized to retrieved this item", HttpStatus.FORBIDDEN);
+        }
+
+        // TODO: Find a more generic or smart way to handle this
+        todo.setTitle(updateTodo.getTitle());
+        todo.setDescription(updateTodo.getDescription());
+        todo.setDuration(updateTodo.getDuration());
+        todo.setPercentage(updateTodo.getPercentage());
+        return ResponseEntity.ok(todo);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> updateTodoItem(Principal principal, @PathVariable int id) {
+        Todo todo = todoRepository.findById(id);
+        if (todo == null) {
+            return new ResponseEntity<>("Todo item with id " + id + " not found", HttpStatus.NOT_FOUND);
+        }
+
+        if (todo.getUser().getId() != currentUser(principal).getId()) {
+            return new ResponseEntity<>("You are not authorized to retrieved this item", HttpStatus.FORBIDDEN);
+        }
+        todoRepository.delete(todo);
+
+        return ResponseEntity.ok("Your request has been successfully handled!");
     }
 
     private UserEntity currentUser(Principal principal) {
