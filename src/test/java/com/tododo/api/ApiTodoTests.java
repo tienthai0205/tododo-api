6ad7e04abb6dc7ec9965c209268917315974bee6
Thai Thuy Tien <tienthai0205>
2021-03-30 20:17:51 +0200

    working docker

--- src/test/java/com/tododo/api/ApiTodoTests.java
+++ src/test/java/com/tododo/api/ApiTodoTests.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
